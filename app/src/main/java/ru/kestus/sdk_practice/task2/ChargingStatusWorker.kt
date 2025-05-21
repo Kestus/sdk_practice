facd83df64625815e00b3982d557e45f0e293887
Kestus <91466473+Kestus@users.noreply.github.com>
2025-05-21 16:48:52 +0300

    task2: use constraint worker instead of receiver

--- app/src/main/java/ru/kestus/sdk_practice/task2/ChargingStatusWorker.kt
+++ app/src/main/java/ru/kestus/sdk_practice/task2/ChargingStatusWorker.kt
@@ -1,8 +1,7 @@
 package ru.kestus.sdk_practice.task2
 
 import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
+import androidx.work.Constraints
 import androidx.work.CoroutineWorker
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkerParameters
@@ -13,15 +12,23 @@ class ChargingStatusWorker(
 ) : CoroutineWorker(context, params) {
 
     override suspend fun doWork(): Result {
-        val intentFilter = IntentFilter().apply {
-            addAction(Intent.ACTION_POWER_CONNECTED)
-        }
-        context.registerReceiver(ChargingStatusReceiver(), intentFilter)
+        AppNotificationManager(context).notify(
+            AppNotificationChannels.CHARGING_STATUS,
+            "Charging Started!"
+        )
         return Result.success()
     }
 
     companion object {
-        fun makeRequest() = OneTimeWorkRequestBuilder<ChargingStatusWorker>().build()
+        fun makeRequest() = OneTimeWorkRequestBuilder<ChargingStatusWorker>().apply {
+            setConstraints(makeConstraints())
+        }.build()
+
+        private fun makeConstraints(): Constraints {
+            return Constraints.Builder()
+                .setRequiresCharging(true)
+                .build()
+        }
     }
 
 
